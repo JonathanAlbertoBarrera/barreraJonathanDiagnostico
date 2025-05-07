926e32304a1bc5fdf9f9a47d44a00147a0dcbff0
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2025-05-07 02:18:10 -0500

    inicioproyecto

---
+++ src/main/java/com/example/diagnosticoJona/DiagnosticoJonaApplication.java
@@ -0,0 +1,13 @@
+package com.example.diagnosticoJona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticoJonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticoJonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/diagnosticoJona/DiagnosticoJonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diagnosticoJona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticoJonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
